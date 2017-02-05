b5876dba7cdcdc9aad283c113037edec287c6190
Aiden Xie <yx79@georgetown.edu>
2017-02-04 19:11:23 -0500

    Update 178_Graph_valid_tree.java

--- LintCode/Lesson2/Union_Find/178_Graph_valid_tree.java
+++ LintCode/Lesson2/Union_Find/178_Graph_valid_tree.java
@@ -17,3 +17,69 @@ Given n = 5 and edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]], return false.
 Tags 
 Depth First Search Facebook Zenefits Union Find Breadth First Search Google
 */
+
+
+
+
+public class Solution {
+    class UnionFind {
+        private int[] father = null;
+        public UnionFind(int n) {
+            father = new int[n]; 
+            for (int i = 0; i < n; i++) {
+                father[i] = -1;
+            }
+        }
+        public int find(int x) {
+            if (father[x] == -1) {
+                return x;
+            }
+            return father[x] = find(father[x]);
+        }
+        public void union(int a, int b) {
+            int root_a = find(a);
+            int root_b = find(b);
+            if (root_a != root_b) {
+                father[root_a] = root_b;
+            }
+        }
+    }
+    /**
+     * @param n an integer
+     * @param edges a list of undirected edges
+     * @return true if it's a valid tree, or false
+     */
+    public boolean validTree(int n, int[][] edges) {
+        if (n == 1 && (edges == null || edges.length == 0)) {
+            // corner case 1, []
+            return true;
+        }
+        if (edges == null || edges.length == 0) {
+            // corner case 2, []
+            return false;
+        }
+        // Write your code here
+        UnionFind union = new UnionFind(n);
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            int root_a = union.find(a);
+            int root_b = union.find(b);
+            if (root_a != root_b) {
+                union.union(a, b);
+            } else {
+                return false;
+            }
+        }
+        // all node should be linked
+        // corner case 3, [[0,1]]
+        int root = union.find(0);
+        for (int i = 1; i < n; i++) {
+            if (root != union.find(i)) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
