813258bfc101ee5b2e2ad765947a255f9a3ad4e5
Aiden Xie <yx79@georgetown.edu>
2017-02-05 12:28:00 -0500

    Update 123_Word_search.java

--- LintCode/Lesson2/Trie/123_Word_search.java
+++ LintCode/Lesson2/Trie/123_Word_search.java
@@ -1,4 +1,14 @@
-/**/
+/*
+ Word Search
+
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" 
+cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+
+
+*/
 
 
 public class Solution {
