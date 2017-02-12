874a9b2429ed10f787daa50bd3c38e6899b480e9
Aiden Xie <yx79@georgetown.edu>
2017-02-12 17:38:53 -0500

    Update 397_Longest_Increasing_continuous_subsequence.java

--- LintCode/Lesson5/397_Longest_Increasing_continuous_subsequence.java
+++ LintCode/Lesson5/397_Longest_Increasing_continuous_subsequence.java
@@ -19,3 +19,44 @@ Enumeration Dynamic Programming Array
 
 */
 
+public class Solution {
+    /**
+     * @param A an array of Integer
+     * @return  an integer
+     */
+    public int longestIncreasingContinuousSubsequence(int[] A) {
+         if (A == null || A.length == 0) {
+             return 0;
+         }
+         int N = A.length;
+         if (N == 1 || N == 2) {
+             return N;
+         }
+         
+         int max = 2, cnt = 1;
+         
+         // left to right
+         for (int i = 0; i < N - 1; i++) {
+             if (A[i] < A[i + 1]) {
+                 cnt++;
+                 max = Math.max(max, cnt);
+             } else {
+                 cnt = 1;
+             }
+         }
+         
+         cnt = 1;
+         // right to left
+         for (int i = N - 1; i > 0; i--) {
+             if (A[i] < A[i - 1]) {
+                 cnt++;
+                 max = Math.max(max, cnt);
+             } else {
+                 cnt = 1;
+             }
+         }
+         
+         return max;
+    }
+    
+}
