b06adec0f52fa9af0c4514f953630daa6d8d05c7
Aiden Xie <yx79@georgetown.edu>
2017-02-12 17:32:31 -0500

    Create 397_Longest_Increasing_continuous_subsequence.java

---
+++ LintCode/Lesson5/397_Longest_Increasing_continuous_subsequence.java
@@ -0,0 +1,21 @@
+/*
+Give an integer array，find the longest increasing continuous subsequence in this array.
+
+An increasing continuous subsequence:
+
+Can be from right to left or from left to right.
+Indices of the integers in the subsequence should be continuous.
+ Notice
+
+O(n) time and O(1) extra space.
+
+Example
+For [5, 4, 2, 1, 3], the LICS is [5, 4, 2, 1], return 4.
+
+For [5, 1, 2, 3, 4], the LICS is [1, 2, 3, 4], return 4.
+
+Tags 
+Enumeration Dynamic Programming Array
+
+*/
+
